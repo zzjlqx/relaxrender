69e63fcc55cc4b3f977255060a13d58ed90a6cbf
VincentCung <326216746@qq.com>
2018-06-15 16:27:52 +0800

    first test

--- features/gamma_correction/render.py
+++ features/gamma_correction/render.py
@@ -1,20 +1,19 @@
-from skimage import data,io  
-import matplotlib.pyplot as plt  
+# from skimage import data, io
+# import matplotlib.pyplot as plt
 
 
 # img = data.coffee()
 # x = img/255
 # gamma = 1/2.2
 # img_new = x**gamma
-# plt.figure()  
-# ax1=plt.subplot(121)  
-# ax2=plt.subplot(122)  
-# ax1.imshow(x)  
-# ax2.imshow(img_new)  
-# plt.show()  
+# plt.figure()
+# ax1=plt.subplot(121)
+# ax2=plt.subplot(122)
+# ax1.imshow(x)
+# ax2.imshow(img_new)
+# plt.show()
 
-class Render:
-    def render_array_to_array(self, img_array,gamma_value):
-        x = img_array/255
-        img_new = x**gamma_value
-        return img_new
+def render_array_to_array( img_array, gamma_value):
+    x = img_array/255
+    img_new = x**gamma_value
+    return img_new
--- features/gamma_correction/test.py
+++ features/gamma_correction/test.py
@@ -1,10 +1,14 @@
 import unittest
 import numpy as np
-from .render import Render
+from .render import *
+from skimage import data
 
 
 
 class Test(unittest.TestCase):
 
     def test_array(self):    #根据接口命名测试的函数名 如 test_size 
-        Render.render_array_to_array()           #函数内调用我们的类 
+        img = data.coffee()
+        render_array_to_array(img,1/2.2)           #函数内调用我们的类 
+        
+        
