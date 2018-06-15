e4dc6692af5e7952193caae042f1c8afb5b1e4bc
cyice <1207201395@qq.com>
2018-06-15 08:38:33 +0800

    把均值去噪和中值去噪分开测试。

--- features/spatial_denoising/clear_noise.py
+++ features/spatial_denoising/clear_noise.py
@@ -5,28 +5,9 @@ from PIL import Image, ImageDraw
 
 class ClearNoise:
 
-    def __init__(self):
-        pass
-
-    # 判断噪点
-    # 将灰度值较大和较小的点认为是噪点
-    def clearNoise_1(self, img, radius):
-        pix = img.load()
-
-        w, h = img.size
-        w -= 1
-        h -= 1
-
-        for i in range(0, w):
-            for j in range(0, h):
-                # 该点的灰度值过大/过小，认为是噪点
-                if pix[i, j][0] < 15 or pix[i, j][0] > 230:
-                    mid = self.mid_of_rec(pix, i, j, radius, w, h)
-                    pix[i, j] = (mid, mid, mid)
-
     # 判断噪点
     # 将灰度值与周围灰度值相似度较小的点认为是噪点
-    def clearNoise_2(self, img, radius, m):
+    def clearNoise(self, img, radius, m,func):
         pix = img.load()
         w, h = img.size
         w -= 1
@@ -46,7 +27,7 @@ class ClearNoise:
                             sim += 1
                 # 该点与周围点相似度较小，认为是噪点
                 if sim < m:
-                    tmp = self.mid_of_rec(pix, i, j, radius, w, h)
+                    tmp = func(pix, i, j, radius, w, h)
                     pix[i, j] = tmp
 
     # 平均值去噪
@@ -62,8 +43,8 @@ class ClearNoise:
                 if j <= 1 or j > h:
                     continue
                 # 排除极值点
-                if pix[i, j][0] > 15 and pix[i, j][0] < 230:
-                    p += pix[i, j][0]
+                if pix[i, j] > 15 and pix[i, j] < 230:
+                    p += pix[i, j]
                     num += 1
         return p // num
 
@@ -96,17 +77,36 @@ class ClearNoise:
         else:
             return (p[m] + p[-m]) // 2
 
-    def clear_noise(self):
+    def clear_noise_mid(self):
 
         # 打开图片
         image = Image.open("./test.jpeg").convert('L')
         img = Image.open("./原图.bmp").convert('L')
 
         # 采用中值去噪
-        self.clearNoise_2(image, 3, 9)
+        self.clearNoise(image, 3, 9,self.mid_of_rec)
 
         # 保存图片
-        image.save("./处理结果.jpeg")
+        image.save("./中值去噪.jpeg")
+        pix_1=image.load()
+        pix_2=img.load()
+
+        image.close()
+        img.close()
+
+        return (pix_1, pix_2,image.size)
+
+    def clear_noise_ave(self):
+
+        # 打开图片
+        image = Image.open("./test.jpeg").convert('L')
+        img = Image.open("./原图.bmp").convert('L')
+
+        # 采用均值去噪
+        self.clearNoise(image, 3, 9,self.ave_of_rec)
+
+        # 保存图片
+        image.save("./均值去噪.jpeg")
         pix_1=image.load()
         pix_2=img.load()
 
--- features/spatial_denoising/test_1.py
+++ features/spatial_denoising/test_1.py
@@ -5,32 +5,36 @@ from features.spatial_denoising.clear_noise import ClearNoise
 
 class TestSpatialDenoising(unittest.TestCase):
 
-    def test_spatail_denoising(self):
+    def test_spatail_denoising_mid(self):
 
         clear = ClearNoise()
 
-        images = clear.clear_noise()
+        images = clear.clear_noise_mid()
+        diff=self.diff_of_images(images)
+        self.assertTrue(diff <= 0.1)
 
-        if len(images)<=1:
-            pix1 = images[0]
-            pix2 = images[1]
+    def test_spatail_denoising_ave(self):
 
-            w, h = images[0].size
-            w -= 1
-            h -= 1
+        clear = ClearNoise()
 
-            tot = 0
+        images = clear.clear_noise_ave()
+        diff=self.diff_of_images(images)
+        self.assertTrue(diff <= 0.1)
 
-            for i in range(0, w):
-                for j in range(0, h):
-                    tmp = abs(pix1[j, i] - pix2[j, i])
-                    tot += tmp / 255
+    # 判断去噪结果和原图的偏差
+    def diff_of_images(self,images):
+        pix1 = images[0]
+        pix2 = images[1]
 
-            t = tot / (w * h)
+        w, h = images[2]
+        tot = 0
 
-            self.assertTrue(t <= 0.1)
+        for i in range(0, w):
+            for j in range(0, h):
+                tmp = abs(pix1[j, i] - pix2[j, i])
+                tot += tmp / 255
 
-
+        return tot / (w * h)
 
 if __name__ == '__main__':
     unittest.main()
