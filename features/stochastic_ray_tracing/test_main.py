d02a46b6ae7fe19aad6913ef73e3183c03bab6af
Sagen <a969256235@vip.qq.com>
2018-06-20 00:38:10 +0800

    1

--- features/stochastic_ray_tracing/test_main.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from stochastic_ray_tracing.main import STRRelaxRender
-
-class TestSTRRelaxRender(unittest.TestCase):
-
-    def test_render(self):
-        test = STRRelaxRender()
-        test.render()
