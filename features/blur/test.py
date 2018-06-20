2275e1e2684fa8ebe0ee9ade277caa3028900736
kobepan <764242499@qq.com>
2018-06-20 22:17:36 +0800

    Update test.py

--- features/blur/test.py
+++ features/blur/test.py
@@ -4,6 +4,21 @@ from blur import *
 
 
 image = cv2.imread("test.jpg")
+
+new_image = blur(image, [(0,0,image.shape[0],image.shape[1])], 20)
+cv2.imshow("image", image)
+cv2.imshow("new", new_image)
+cv2.waitKey()
+
+new_image = blur_version2(image, [(0,0,image.shape[0],image.shape[1])], 10)
+cv2.imshow("image", image)
+cv2.imshow("new", new_image)
+cv2.waitKey()
+
+
+
+
+image = cv2.imread("test.jpg")
 districts = [(800,1400,400,400)]
 new_image = blur(image, districts, 10)
 
@@ -45,19 +60,6 @@ cv2.waitKey()
 
 
 
-
-
-image = cv2.imread("test.jpg")
-
-new_image = blur(image, [(0,0,image.shape[0],image.shape[1])], 20)
-cv2.imshow("image", image)
-cv2.imshow("new", new_image)
-cv2.waitKey()
-
-new_image = blur_version2(image, [(0,0,image.shape[0],image.shape[1])], 10)
-cv2.imshow("image", image)
-cv2.imshow("new", new_image)
-cv2.waitKey()
 
 
 
