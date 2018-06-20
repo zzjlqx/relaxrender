37096a338a9cef7209a8da5e7569080974a759ba
bangbang6 <32785707+bangbang6@users.noreply.github.com>
2018-06-20 22:19:22 +0800

    Update test.py

--- features/blur/test.py
+++ features/blur/test.py
@@ -2,22 +2,6 @@ import numpy as np
 import cv2
 from blur import *
 
-
-image = cv2.imread("test.jpg")
-
-new_image = blur(image, [(0,0,image.shape[0],image.shape[1])], 20)
-cv2.imshow("image", image)
-cv2.imshow("new", new_image)
-cv2.waitKey()
-
-new_image = blur_version2(image, [(0,0,image.shape[0],image.shape[1])], 10)
-cv2.imshow("image", image)
-cv2.imshow("new", new_image)
-cv2.waitKey()
-
-
-
-
 image = cv2.imread("test.jpg")
 districts = [(800,1400,400,400)]
 new_image = blur(image, districts, 10)
@@ -56,6 +40,22 @@ for district in districts:
 	i += 1
 
 cv2.waitKey()
+
+
+
+
+image = cv2.imread("test.jpg")
+
+new_image = blur(image, [(0,0,image.shape[0],image.shape[1])], 20)
+cv2.imshow("image", image)
+cv2.imshow("new", new_image)
+cv2.waitKey()
+
+new_image = blur_version2(image, [(0,0,image.shape[0],image.shape[1])], 10)
+cv2.imshow("image", image)
+cv2.imshow("new", new_image)
+cv2.waitKey()
+
 
 
 
