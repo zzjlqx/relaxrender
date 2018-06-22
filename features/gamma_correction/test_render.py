6950f378d0e8a74abf48dd1a151d779c0ea838b4
Endless-Run <884273322@qq.com>
2018-06-22 11:55:15 +0800

    Some small fixes

--- features/gamma_correction/test_render.py
+++ features/gamma_correction/test_render.py
@@ -7,7 +7,7 @@ from skimage import data
 class Test(unittest.TestCase):
 
     def test_gamma(self):    
-        img = data.coffee()                  #测试图片用例
+        img = data.coffee()                  #测试图片
         gamma_correction(img,2.2,1,1,1)           #函数内调用我们的类 
         gamma_correction(img,2.2,2)
         gamma_correction(img,2.2,3)
